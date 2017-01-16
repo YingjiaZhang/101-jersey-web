3e092105240814c651eab1cce2f863b921d63b7f
YingjiaZhang <zhyingjia@163.com>
2017-01-16 20:08:59 +0800

    add flywaymigrate

---
+++ src/main/resources/db/migration/V2__Add_test_data_for_item_table.sql
@@ -0,0 +1,3 @@
+truncate item;
+insert into item values(null,10.0,"apple");
+insert into item values(null,5.0,"pen");
---
+++ src/main/resources/db/migration/V4__Add_test_data_for_category_table.sql
@@ -0,0 +1,4 @@
+truncate category;
+insert into category values(null,"all");
+insert into category values(null,"fruits");
+insert into category values(null,"tools");
